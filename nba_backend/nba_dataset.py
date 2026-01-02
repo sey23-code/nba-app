217fa7aa84939aa43708c69cc5e89ea748832e9d
Seydina <Seydina@MacBook-Air.local>
2026-01-01 19:29:54 -0500

    Implemented Logistic Regression model using added features

--- nba_backend/nba_dataset.py
+++ nba_backend/nba_dataset.py
@@ -6,7 +6,7 @@ games = leaguegamefinder.LeagueGameFinder(
     season_nullable="2025-26",
     season_type_nullable="Regular Season").get_data_frames()[0]
 games = games.sort_values(["TEAM_ID", "GAME_DATE"]).reset_index(drop=True)
-games["is_home"] = games["MATCHUP"].apply(lambda x: "Y" if isinstance(x, str) and "vs." in x else "N")
+games["is_home"] = games["MATCHUP"].apply(lambda x: 1 if isinstance(x, str) and "vs." in x else 0)
 
 #Calculate rolling stats of teams (15 games)
 group_recent_games = games.groupby("TEAM_ID")
@@ -31,37 +31,71 @@ games["avg_tov_last_15"] = group_recent_games["TOV"].transform(lambda x: x.rolli
 #Rolling Fg Made
 games["avg_fgm_last_15"] = group_recent_games["FGM"].transform(lambda x: x.rolling(window=15).mean().shift(1)).round(1)
 
+
+#Calculate rolling stats of teams (5 games)
+#Rolling Avg Pts
+games["avg_pts_last_5"] = group_recent_games["PTS"].transform(lambda x: x.rolling(window=5).mean().shift(1)).round(1)
+#Rolling Avg Reb
+games["avg_reb_last_5"] = group_recent_games["REB"].transform(lambda x: x.rolling(window=5).mean().shift(1)).round(1)
+#Rolling Avg Ast
+games["avg_ast_last_5"] = group_recent_games["AST"].transform(lambda x: x.rolling(window=5).mean().shift(1)).round(1)
+#Rolling Stl Ast
+games["avg_stl_last_5"] = group_recent_games["STL"].transform(lambda x: x.rolling(window=5).mean().shift(1)).round(1)
+#Rolling Blk Ast
+games["avg_blk_last_5"] = group_recent_games["BLK"].transform(lambda x: x.rolling(window=5).mean().shift(1)).round(1)
+#Rolling Avg Fg Pct
+games["avg_fgpct_last_5"] = (group_recent_games["FG_PCT"].transform(lambda x: x.rolling(window=5).mean().shift(1)) * 100).round(1)
+#Rolling Avg +/-
+games["avg_pm_last_5"] = group_recent_games["PLUS_MINUS"].transform(lambda x: x.rolling(window=5).mean().shift(1)).round(1)
+#Rolling 3pt Made
+games["avg_3pm_last_5"] = group_recent_games["FG3M"].transform(lambda x: x.rolling(window=5).mean().shift(1)).round(1)
+#Rolling Turnovers
+games["avg_tov_last_5"] = group_recent_games["TOV"].transform(lambda x: x.rolling(window=5).mean().shift(1)).round(1)
+#Rolling Fg Made
+games["avg_fgm_last_5"] = group_recent_games["FGM"].transform(lambda x: x.rolling(window=5).mean().shift(1)).round(1)
+
 #lakers = games[games["TEAM_ID"] == 1610612747]
-#print(lakers[["GAME_DATE", "PTS", "avg_pm_last_15", "WL"]])
+#print(lakers[["avg_pts_last_5", "avg_reb_last_5"]])
 
 predictive_columns = [
     "TEAM_ID",
     "GAME_ID",
     "GAME_DATE",
     "MATCHUP",
+    "is_home",
     "avg_pts_last_15",
+    "avg_pts_last_5",
     "avg_reb_last_15",
+    "avg_reb_last_5",
     "avg_ast_last_15",
+    "avg_ast_last_5",
     "avg_stl_last_15",
+    "avg_stl_last_5",
     "avg_blk_last_15",
+    "avg_blk_last_5",
     "avg_fgpct_last_15",
+    "avg_fgpct_last_5",
     "avg_pm_last_15",
+    "avg_pm_last_5",
     "avg_fgm_last_15",
+    "avg_fgm_last_5",
     "avg_tov_last_15",
+    "avg_tov_last_5",
     "avg_3pm_last_15",
-    "WL",
-    "is_home"
+    "avg_3pm_last_5",
+    "WL"
     ]
 
 games = games[predictive_columns]
-home_games = games[games["is_home"] == "Y"]
-away_games = games[games["is_home"] == "N"]
+home_games = games[games["is_home"] == 1]
+away_games = games[games["is_home"] == 0]
 merged_games = pd.merge(home_games, away_games, on="GAME_ID", suffixes=("_HOME", "_AWAY"))
 merged_games["HOME_WIN"] = merged_games["WL_HOME"].apply(lambda x: 1 if x == "W" else 0)
-merged_games = merged_games.drop(columns=["WL_HOME", "WL_AWAY"])
+merged_games = merged_games.drop(columns=["WL_HOME", "WL_AWAY", "GAME_DATE_AWAY"])
 merged_games = merged_games.dropna()
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 pd.set_option('display.max_rows', 100)
-print(merged_games.head(30))
+
+merged_games.to_csv("nba_training_data.csv", index=False)
 
---
+++ nba_backend/nba_model.py
@@ -0,0 +1,92 @@
+import pandas as pd
+import numpy as np
+from datetime import datetime
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.ensemble import RandomForestClassifier
+
+data = pd.read_csv('nba_training_data.csv')
+#Sorting data by date
+data["GAME_DATE_HOME"] = pd.to_datetime(data["GAME_DATE_HOME"])
+data = data.sort_values('GAME_DATE_HOME')
+
+X = data.drop(columns=["TEAM_ID_HOME", "GAME_ID", "GAME_DATE_HOME", "MATCHUP_HOME", "TEAM_ID_AWAY", 
+                       "MATCHUP_AWAY", "is_home_HOME", "is_home_AWAY", "HOME_WIN"])
+y = data["HOME_WIN"]
+
+#Momentum variables
+data["momentum_pts_HOME"] = data["avg_pts_last_5_HOME"] - data["avg_pts_last_15_HOME"]
+data["momentum_pts_AWAY"] = data["avg_pts_last_5_AWAY"] - data["avg_pts_last_15_AWAY"]
+data["momentum_reb_HOME"] = data["avg_reb_last_5_HOME"] - data["avg_reb_last_15_HOME"]
+data["momentum_reb_AWAY"] = data["avg_reb_last_5_AWAY"] - data["avg_reb_last_15_AWAY"]
+data["momentum_ast_HOME"] = data["avg_ast_last_5_HOME"] - data["avg_ast_last_15_HOME"]
+data["momentum_ast_AWAY"] = data["avg_ast_last_5_AWAY"] - data["avg_ast_last_15_AWAY"]
+data["momentum_stl_HOME"] = data["avg_stl_last_5_HOME"] - data["avg_stl_last_15_HOME"]
+data["momentum_stl_AWAY"] = data["avg_stl_last_5_AWAY"] - data["avg_stl_last_15_AWAY"]
+data["momentum_blk_HOME"] = data["avg_blk_last_5_HOME"] - data["avg_blk_last_15_HOME"]
+data["momentum_blk_AWAY"] = data["avg_blk_last_5_AWAY"] - data["avg_blk_last_15_AWAY"]
+data["momentum_fgpct_HOME"] = data["avg_fgpct_last_5_HOME"] - data["avg_fgpct_last_15_HOME"]
+data["momentum_fgpct_AWAY"] = data["avg_fgpct_last_5_AWAY"] - data["avg_fgpct_last_15_AWAY"]
+data["momentum_pm_HOME"] = data["avg_pm_last_5_HOME"] - data["avg_pm_last_15_HOME"]
+data["momentum_pm_AWAY"] = data["avg_pm_last_5_AWAY"] - data["avg_pm_last_15_AWAY"]
+data["momentum_fgm_HOME"] = data["avg_fgm_last_5_HOME"] - data["avg_fgm_last_15_HOME"]
+data["momentum_fgm_AWAY"] = data["avg_fgm_last_5_AWAY"] - data["avg_fgm_last_15_AWAY"]
+data["momentum_tov_HOME"] = data["avg_tov_last_5_HOME"] - data["avg_tov_last_15_HOME"]
+data["momentum_tov_AWAY"] = data["avg_tov_last_5_AWAY"] - data["avg_tov_last_15_AWAY"]
+data["momentum_3pm_HOME"] = data["avg_3pm_last_5_HOME"] - data["avg_3pm_last_15_HOME"]
+data["momentum_3pm_AWAY"] = data["avg_3pm_last_5_AWAY"] - data["avg_3pm_last_15_AWAY"]
+
+#Trend variables
+data["trend_pts_HOME"] = data["avg_pts_last_5_HOME"] / data["avg_pts_last_15_HOME"]
+data["trend_pts_AWAY"] = data["avg_pts_last_5_AWAY"] / data["avg_pts_last_15_AWAY"]
+data["trend_reb_HOME"] = data["avg_reb_last_5_HOME"] / data["avg_reb_last_15_HOME"]
+data["trend_reb_AWAY"] = data["avg_reb_last_5_AWAY"] / data["avg_reb_last_15_AWAY"]
+data["trend_ast_HOME"] = data["avg_ast_last_5_HOME"] / data["avg_ast_last_15_HOME"]
+data["trend_ast_AWAY"] = data["avg_ast_last_5_AWAY"] / data["avg_ast_last_15_AWAY"]
+data["trend_stl_HOME"] = data["avg_stl_last_5_HOME"] / data["avg_stl_last_15_HOME"]
+data["trend_stl_AWAY"] = data["avg_stl_last_5_AWAY"] / data["avg_stl_last_15_AWAY"]
+data["trend_blk_HOME"] = data["avg_blk_last_5_HOME"] / data["avg_blk_last_15_HOME"]
+data["trend_blk_AWAY"] = data["avg_blk_last_5_AWAY"] / data["avg_blk_last_15_AWAY"]
+data["trend_fgpct_HOME"] = data["avg_fgpct_last_5_HOME"] / data["avg_fgpct_last_15_HOME"]
+data["trend_fgpct_AWAY"] = data["avg_fgpct_last_5_AWAY"] / data["avg_fgpct_last_15_AWAY"]
+data["trend_pm_HOME"] = data["avg_pm_last_5_HOME"] / data["avg_pm_last_15_HOME"]
+data["trend_pm_AWAY"] = data["avg_pm_last_5_AWAY"] / data["avg_pm_last_15_AWAY"]
+data["trend_fgm_HOME"] = data["avg_fgm_last_5_HOME"] / data["avg_fgm_last_15_HOME"]
+data["trend_fgm_AWAY"] = data["avg_fgm_last_5_AWAY"] / data["avg_fgm_last_15_AWAY"]
+data["trend_tov_HOME"] = data["avg_tov_last_5_HOME"] / data["avg_tov_last_15_HOME"]
+data["trend_tov_AWAY"] = data["avg_tov_last_5_AWAY"] / data["avg_tov_last_15_AWAY"]
+data["trend_3pm_HOME"] = data["avg_3pm_last_5_HOME"] / data["avg_3pm_last_15_HOME"]
+data["trend_3pm_AWAY"] = data["avg_3pm_last_5_AWAY"] / data["avg_3pm_last_15_AWAY"]
+
+data = data.replace([np.inf, -np.inf], np.nan)
+
+#Spliting games -> 80% train / 20% test
+split_index = int(len(data) * 0.8)
+#Using 199 games to train
+X_train = X.iloc[:split_index]
+X_test = X.iloc[split_index:] #Rest is testing games
+y_train = y.iloc[:split_index]
+y_test = y.iloc[split_index:]
+
+#Logistic Regression algorithm
+model = LogisticRegression(max_iter=5000)
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+report = classification_report(y_test, y_pred)
+#48% Accuracy score
+print(f"Accuracy score: {accuracy * 100}%")
+#print(report)
+
+print(f"y_pred distribution:\n{pd.Series(y_pred).value_counts()}")
+print("\nClassification Report:")
+print(classification_report(y_test, y_pred))
+
+#Random Forest algorithm
+#model2 = RandomForestClassifier(n_estimators=500, random_state=42)
+#model2.fit(X_train, y_train)
+#y_pred = model2.predict(X_test)
+#accuracy = accuracy_score(y_pred, y_test)
+#48% -> 50% Accuracy score
+#print(f"Accuracy score: {accuracy * 100}%")
